3823255dbfc2c368e2fa38d414850d4f8d89f254
SM <ixpata@gmail.com>
2014-07-15 17:48:03 -0400

    cleaned up stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,17 +1,14 @@
 def stock_picker(prices)
   profits = {}
-  #iterate through prices to substract each day from other days
+  
   i = 0
   while i <= prices.length
     x = (prices[i+1].to_i) - (prices[i].to_i)
     profits[x] = [i, i+1] 
     i+=1
   end 
-    #add profits as key (difference between day 
-  #bought and day sold with day bought and day sold as values)
+    
   print profits.max[1]
-  #a = profits.keys.sort.each {|key| puts profits[key]} #put profits in order from lowest to highest
-  #puts profits[a[-1]] #returns highest profit days to buy and sell
 
 end
 
